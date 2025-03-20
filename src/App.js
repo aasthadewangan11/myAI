a219ae31adb0261d97d81e58a0e8cd714deba179
Aastha Dewangan <aasthadewangan11@gmail.com>
2025-03-21 00:36:03 +0530

    demo

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
+  const [question, setQuestion] = useState('');
+  const submitHandler = (e) => {
+    e.preventDefault();
+    console.log(question);
+    axios.post('https://gemini-app-delta.vercel.app/getResponse',{
+     question:question
+     .then(res => {
+      console.log(res.data);
+  })
+  .catch(err => {
+      console.log(err);
+  })
+  
+    })
+  };
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="user">
+        <div className="profile-pic">
+          <img className='pic' alt='profile-pic' src={require('../src/assets/aastha.jpeg')} />
+        </div>
+        <p className="label">Question</p>
+        <textarea onChange={(e) => setQuestion(e.target.value)} />
+        <button onClick={submitHandler}>Send</button>
+      </div>
+
+      <div className="gemini">
+        <div className="profile-pic">
+          <img className='pic' alt='profile-pic' src={require('../src/assets/gemini.jpg')} />
+        </div>
+        <p className="label">Response</p>
+        <textarea></textarea>
+        <button>Speak</button>
+      </div>
     </div>
   );
 }
